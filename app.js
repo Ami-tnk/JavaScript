068cf90985ec3b0cc274866e27fe8ef3462f00b9
Ami-tnk <ami1y251992@gmail.com>
2020-12-07 13:33:38 +0900

    条件分岐

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert('Hello Wold');
+var orange = 100;
+var apple = 120;
+
+if(orange < apple ){
+  alert('みかんの値段がリンゴより安い');
+} else if(orange == apple){
+  alert('みかんとリンゴが同じ値段');
+} else{
+  alert('みかんの値段がリンゴより高い');
+}
